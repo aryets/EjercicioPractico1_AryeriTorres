767a400ca33261c598449a03ffa4682a215d05f0
Aryeri Torres <yeye120625@gmail.com>
2026-06-24 21:34:59 -0600

    2

--- GimnasioFitness/src/main/java/GimnasioFitness/GimnasioFitnessApplication.java
+++ GimnasioFitness/src/main/java/GimnasioFitness/GimnasioFitnessApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GimnasioFitnessApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GimnasioFitnessApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GimnasioFitnessApplication.class, args);
+    }
 }
